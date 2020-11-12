da42f391c3a36d8fceb6f273fcf41f1bd4a4b00d
s.seget <s.seget@avsystem.com>
2020-11-12 22:59:10 +0100

    Init

---
+++ Assets/CubeHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Specialized;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeHandler : MonoBehaviour
+{
+
+    public GameObject RightController;
+    public Rigidbody Ball;
+    public TextMesh Text; 
+    public TextMesh TextButton;
+
+    private OrderedDictionary rounds = new OrderedDictionary();
+    private int actualRound = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+     rounds.Add(1, "");
+     rounds.Add(2, "");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = RightController.transform.position.x.ToString();
+        // if (RightController.transform.position.x > 0) {
+        //     transform.Rotate(0.0f, 0.0f, 0.1f, Space.Self);
+        // } else {
+        //     transform.Rotate(0.0f, 0.0f, -0.1f, Space.Self);
+        // }
+        transform.Rotate(0.0f, 0.0f, 0.3f * RightController.transform.position.x, Space.Self);
+
+        // bool pressed = OVRInput.Get(OVRInput.Button.One);
+
+        // if(pressed) {
+        //     TextButton.text = "wciśniety";
+        // } else {
+        //     TextButton.text = "nie";
+        // }
+
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesWithRole(UnityEngine.XR.InputDeviceRole.RightHanded, inputDevices);
+
+        var device = inputDevices[0];
+
+        bool triggerValue = false;
+        device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue);
+        if(triggerValue) {
+            TextButton.text = "zajebiście";
+            Ball.transform.position = new Vector3(0.0f, 30.0f, 7.0f);
+            Ball.velocity = Vector3.zero;
+            Ball.angularVelocity = Vector3.zero;
+        } else {
+            TextButton.text = "chujowo";
+        }
+
+        //Debug.Log(RightController.transform.position);
+    }
+}
---
+++ Assets/GameHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data.Common;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameHandler : MonoBehaviour
+{
+
+    public GameObject RightController;
+    public GameObject LeftController;
+    public Rigidbody Ball;
+    public TextMesh RightControllerInfo; 
+    public TextMesh LeftControllerInfo;
+    public TextMesh TextButton;
+
+    public GameObject round1Cube;
+    public GameObject round2Cube;
+    public GameObject round3Cube;
+
+    private OrderedDictionary rounds = new OrderedDictionary();
+    private int actualRound = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rounds.Add(0, round1Cube);
+        rounds.Add(1, round2Cube);
+        rounds.Add(2, round3Cube);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rightPosition = RightController.transform.position.y + 0.4f;
+        var leftPosition = LeftController.transform.position.y + 0.4f;
+        RightControllerInfo.text = rightPosition.ToString();
+        LeftControllerInfo.text = leftPosition.ToString();
+
+        ((GameObject)rounds[(object)actualRound]).transform.Rotate(0.0f, 0.0f, 0.3f * rightPosition, Space.Self);
+        ((GameObject)rounds[(object)actualRound]).transform.Rotate(0.3f * leftPosition, 0.0f, 0.0f, Space.Self);
+
+
+        var inputDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesWithRole(UnityEngine.XR.InputDeviceRole.RightHanded, inputDevices);
+
+        var device = inputDevices[0];
+
+        bool triggerValue = false;
+        device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue);
+        if(triggerValue) {
+            TextButton.text = "zajebiście";
+            resetBall();
+        } else {
+            TextButton.text = "chujowo";
+        }
+        checkIfGameOver();
+    }
+
+    void resetBall() {
+        Ball.transform.position = new Vector3(0.0f, 30.0f, 7.0f);
+        Ball.velocity = Vector3.zero;
+        Ball.angularVelocity = Vector3.zero;
+    }
+
+    void checkIfGameOver() {
+        if (Ball.transform.position.y < -100.0f) {
+            nextRound();
+            TextButton.text = "Nastepna runda.";
+            resetBall();
+        }
+    }
+
+    void nextRound() {
+        ((GameObject)rounds[(object)actualRound]).transform.position = new Vector3(0.0f, -4.0f, -30.0f);
+        actualRound = (actualRound + 1) % 3; 
+        ((GameObject)rounds[(object)actualRound]).transform.position = new Vector3(0.0f, -4.0f, 7.0f);
+    }
+}
+
